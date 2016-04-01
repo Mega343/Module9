f78a79cc1a6593d340932e4be4e9abf3d8b3add0
Mega343 <mega9594@Rambler.ru>
2016-04-01 19:10:51 +0300

    Create Romashka.java

---
+++ Romashka.java
@@ -0,0 +1,62 @@
+
+public class Romashka extends Flower implements Comparable<Flower> {
+
+   private String color;
+   private int price;
+
+    @Override
+    public int getLenght() {
+        return lenght;
+    }
+
+    @Override
+    public void setLenght(int lenght) {
+        this.lenght = lenght;
+    }
+
+    @Override
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    private int lenght;
+
+
+
+    public Romashka(String color, int lenght, int price) {
+        this.color = color;
+        this.lenght = lenght;
+        this.price = price;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Romashka " +
+                "color='" + color + '\'' +
+                ", price=" + price +
+                ", lenght=" + lenght
+                ;
+    }
+
+    @Override
+    public int compareTo(Flower o) {
+        return this.getPrice() - o.getPrice();
+    }
+}
