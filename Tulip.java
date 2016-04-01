0192f44427a93d74573230309741180b8b1c832e
Mega343 <mega9594@Rambler.ru>
2016-04-01 19:13:06 +0300

    Create Tulip.java

---
+++ Tulip.java
@@ -0,0 +1,57 @@
+
+public class Tulip extends Flower implements Comparable<Flower> {
+
+    private String color;
+    private int price;
+    private int lenght;
+
+
+    public Tulip(String color, int lenght, int price) {
+        this.color = color;
+        this.lenght = lenght;
+        this.price = price;
+    }
+
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public int getLenght() {
+        return lenght;
+    }
+
+    @Override
+    public void setLenght(int lenght) {
+        this.lenght = lenght;
+    }
+
+    @Override
+    public String toString() {
+        return "Tulip " +
+                "color='" + color + '\'' +
+                ", price=" + price +
+                ", lenght=" + lenght;
+    }
+
+    @Override
+    public int compareTo(Flower o) {
+        return this.getPrice() - o.getPrice();
+    }
+}
