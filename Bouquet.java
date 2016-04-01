66ef55beec4c7c22a30353945abdbfa55221aa34
Mega343 <mega9594@Rambler.ru>
2016-04-01 19:09:04 +0300

    Create Bouquet.java

---
+++ Bouquet.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+
+public class Bouquet {
+
+    HashSet<Flower> list;
+
+    public Bouquet(Flower flower) {
+        list.add(flower);
+    }
+
+    public Bouquet(HashSet<Flower> name) {
+        if (!name.isEmpty())
+            list.addAll(name);
+        // else
+        // error
+    }
+
+}
