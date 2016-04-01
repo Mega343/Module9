183627d86b28a7542ce9749dc75e6a253df66698
Mega343 <mega9594@Rambler.ru>
2016-04-01 19:09:36 +0300

    Create Flower.java

---
+++ Flower.java
@@ -0,0 +1,30 @@
+public class Flower {
+
+ private    String color;
+ private    int price;
+ private    int lenght;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getLenght() {
+        return lenght;
+    }
+
+    public void setLenght(int lenght) {
+        this.lenght = lenght;
+    }
+}
